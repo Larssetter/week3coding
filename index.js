04696a326c0c97fb4f16f1e47261088a36689483
lars setterstrom <lars.setter@gmail.com>
2022-11-01 22:05:14 -0500

    week 3 coding

---
+++ index.js
@@ -0,0 +1,106 @@
+console.log("test");
+ //coding assignment week 3 
+
+ //needs work, I need to go over the video answers key that was given. 
+
+ //1. 
+ let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+   console.log(ages[0 - 7]);
+   console.log(ages.push(94));
+   for (let i = 0; i < ages.length; i++ )
+   console.log(ages[i]);
+
+   //2.
+   let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
+
+        for (let i = o; i < names.length; i++) {
+
+           //not sure on how to concatenate for loop??
+        }
+
+    //3.
+        let newArray = ["lab", "coat", "jacket"];
+        console.log(newArray.length - 1); // to get last index of array, one method 
+
+    //4.
+        //first element of array 
+       unshift() //one method to find the index or 0 based in array
+
+    //5.
+        let nameLengths= ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+            for (let i = 0; i <= nameLengths.length; i++);
+            return // not sure on this 
+
+
+    
+    //6.
+         for (let i = 0; i <= nameLengths.length; i++ )
+            sum = sum + nameLengths[i];
+            //not sure on this yet 
+        
+    //.7
+        function myFunction(word, n) {
+
+            return word *= 3; 
+        }
+
+    //8.
+        function anotherFunction(firstName, lastName) {
+
+            return firstName + lastName;
+        }
+            anotherFunction(lars, setterstrom);
+        
+
+    //9.
+    
+       function arrayOfNums(arr) {
+             //need help on this 
+     
+    //.10
+        function arrayOfNumbers(arr) {
+
+
+        }
+
+ 
+        //.11
+
+
+        //12.
+            function willBuyDrink(isHotOutside, moneyInPocket)  {
+
+                if (moneyInPocket && isHotOutside > 10.50) {
+                    return true;
+                } else {
+
+                    return false; 
+                }
+            }
+
+        }
+                
+                 
+        //13.
+
+            function enoughGroceryMoney(x) {
+
+                if (enoughGroceryMoney > x) {
+
+                    console.log("purchase this now")
+                } else {
+
+                    console.log("do not purchase this now");
+                }
+            }   
+                enoughGroceryMoney(10.00);
+
+
+            
+            
+
+             
+                
